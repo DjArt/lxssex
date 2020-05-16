d7e5c6ad048c8e653ff988109e25eb4ef1d5b639
djart <art.letter.mail@gmail.com>
2020-05-16 19:21:33 +0800

    Unmanaged export attribute

---
+++ libEGL.Linux/UnmanagedCallersOnlyAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Runtime.InteropServices
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class UnmanagedCallersOnlyAttribute : Attribute
+    {
+        public string EntryPoint;
+        public CallingConvention CallingConvention;
+
+        public UnmanagedCallersOnlyAttribute() { }
+    }
+}
