79826a495938e950fb61128b17f565dd17368fd6
djart <art.letter.mail@gmail.com>
2020-05-19 19:43:39 +0800

    RPCServicingAttribute added

---
+++ src/Shared/lxssex.RPC/Attributes/RPCServicingAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lxssex.RPC.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class RPCServicingAttribute : Attribute
+    {
+    }
+}
