3b0d70102095876e6e7276e1a8b5f24dca50e378
djart <art.letter.mail@gmail.com>
2020-05-19 19:42:04 +0800

    API subset of EGLenum added

---
+++ src/Shared/libEGL.Shared/API.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EGL
+{
+    public enum API : uint
+    {
+        OpenGL = 12450,
+        OpenGLES = 12448,
+        OpenVG = 12449
+    }
+}
