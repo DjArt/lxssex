d83f5b87d854d3b2c1195b308378a890740802a1
djart <art.letter.mail@gmail.com>
2020-05-22 02:37:59 +0800

    Target renamed to TargetType

---
+++ src/Shared/libEGL.Shared/TargetType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EGL
+{
+    public enum TargetType : uint
+    {
+        Texture2D = 0x30B1,
+        Texture3D = 0x30B2,
+        TextureCubeMapPositiveX = 0x30B3,
+        TextureCubeMapNegativeX = 0x30B4,
+        TextureCubeMapPositiveY = 0x30B5,
+        TextureCubeMapNegativeY = 0x30B6,
+        TextureCubeMapPositiveZ = 0x30B7,
+        TextureCubeMapNegativeZ = 0x30B8,
+        RenderBuffer = 0x30B9
+    }
+}
