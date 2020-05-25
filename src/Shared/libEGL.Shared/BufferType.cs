d9786c469de80007930f63e7c06ca7f47fe3d703
djart <art.letter.mail@gmail.com>
2020-05-26 02:09:30 +0800

    Files was moved

--- src/Shared/libEGL.Shared/BufferType.cs
+++
@@ -1,9 +0,0 @@
-﻿using static EGL.Constants;
-
-namespace EGL
-{
-    public enum BufferType : uint
-    {
-        OpenVGImage = EGL_OPENVG_IMAGE,
-    }
-}
--- src/Shared/libEGL.Shared/WaitNativeEngine.cs
+++
@@ -1,9 +0,0 @@
-﻿using static EGL.Constants;
-
-namespace EGL
-{
-    public enum WaitNativeEngine : uint
-    {
-        CoreNativeEngine = EGL_CORE_NATIVE_ENGINE,
-    }
-}
