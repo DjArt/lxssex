78df448c29a4245c4711fe10e3fe22549882cf50
djart <art.letter.mail@gmail.com>
2020-06-03 03:03:44 +0000

    driver init routine changes for lxssex.Service

--- src/Kernel/lxssex/driverentry.cpp
+++ src/Kernel/lxssex/driverentry.cpp
@@ -1,7 +1,11 @@
 #include "vfsplugin.hpp"
+#include "service/lxssex.Service.Linux.hpp"
+#include "service/lxssex.Service.Shared.hpp"
+#include "service/lxssex.Service.Windows.hpp"
 
 INT InitializationRoutine(_In_ PLX_INSTANCE Instance)
 {
+    LinuxServiceInitialize(Instance);
     return LoadPlugins(Instance);
 }
 
@@ -14,9 +18,15 @@ EXTERN_C NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_ST
         .MountNamespaceInitialize = InitializationRoutine
     };
     status = LxInitialize(DriverObject, &subsystemInformation);
-    if (status != STATUS_TOO_LATE)
-    {
-        return status;
-    }
+    if (status == STATUS_TOO_LATE) status = STATUS_SUCCESS;
+
+    status = WindowsServiceInitializate(DriverObject, RegistryPath);
+    DriverObject->DriverUnload = (PDRIVER_UNLOAD)&WindowsServiceInitializate;
+    DriverObject->MajorFunction[IRP_MJ_CLEANUP] = (PDRIVER_DISPATCH)&WindowsServiceCleanup;
+    DriverObject->MajorFunction[IRP_MJ_CLOSE] = (PDRIVER_DISPATCH)&WindowsServiceClose;
+    DriverObject->MajorFunction[IRP_MJ_CREATE] = (PDRIVER_DISPATCH)&WindowsServiceCreate;
+    DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = (PDRIVER_DISPATCH)&WindowsServiceIOControl;
+    DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = (PDRIVER_DISPATCH)&WindowsServiceIOControl;
+
     return STATUS_SUCCESS;
 }
