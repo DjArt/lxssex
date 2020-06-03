ae847c7eff58a4e11606541c15c81872794da122
djart <art.letter.mail@gmail.com>
2020-06-03 03:02:43 +0000

    vfsplugin reworked, still WIP

---
+++ src/Kernel/lxssex/include/km/vfsplugin.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <lxcore.h>
+
+typedef struct _VFS_PLUGIN
+{
+	PUNICODE_STRING PluginName;
+} VFS_PLUGIN, * PVFS_PLUGIN;
+
+typedef struct _VFS_PLUGIN_CALLBACKS
+{
+	PVFS_MOUNT_NAMESPACE_INITIALIZE_CALLBACK InitializationCallback;
+} VFS_PLUGIN_CALLBACKS, * PVFS_PLUGIN_CALLBACKS;
+
+EXTERN_C NTSTATUS VFSRegisterPlugin(_In_ PVFS_PLUGIN Plugin, _In_ PVFS_PLUGIN_CALLBACKS Callbacks);
--- src/Kernel/lxssex/vfsplugin.hpp
+++ src/Kernel/lxssex/vfsplugin.hpp
@@ -2,15 +2,18 @@
 
 #include <lxcore.h>
 
-#include "include/vfsplugin.h"
+#include "include/km/vfsplugin.h"
+
+typedef struct _VFS_PLUGIN_ENTRY
+{
+	VFS_PLUGIN Plugin;
+	VFS_PLUGIN_CALLBACKS Callbacks;
+} VFS_PLUGIN_ENTRY, * PVFS_PLUGIN_ENTRY;
 
 typedef struct _VFS_PLUGIN_SETTINGS
 {
-	PVFS_PLUGIN Plugin;
+	PVFS_PLUGIN_ENTRY PluginEntry;
 	BOOLEAN IsEnabled;
 } VFS_PLUGIN_SETTINGS, * PVFS_PLUGIN_SETTINGS;
-
-extern PVFS_PLUGIN_SETTINGS PluginStore;
-extern SIZE_T PluginStoreLength;
 
 INT LoadPlugins(_In_ PLX_INSTANCE Instance);
