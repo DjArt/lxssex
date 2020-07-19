b1a7cd8ddec1018acc6ffcc683af6df5f15cd166
djart <art.letter.mail@gmail.com>
2020-07-19 03:43:23 +0000

    lxssex.WindowManager.Linux layout added

---
+++ src/Linux/lxssex.WindowManager.Linux/clean.sh
@@ -0,0 +1,3 @@
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
+dotnet clean -r $1 -c $2
---
+++ src/Linux/lxssex.WindowManager.Linux/lxssex.WindowManager.Linux.h
@@ -0,0 +1,8 @@
+﻿typedef void* DisplayHandle;
+
+DisplayHandle CreateWindow();
+bool GetWindowSize(DisplayHandle display, int* x, int* y);
+bool SetWindowSize(DisplayHandle display, int x, int y);
+bool SetWindowTitle(DisplayHandle display, char* string);
+bool SetIcon(DisplayHandle display, void* buffer, int size);
+bool CloseWindow(DisplayHandle display);
