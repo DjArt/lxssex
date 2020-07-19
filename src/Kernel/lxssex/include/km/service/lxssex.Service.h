c05a149ddfcc0f49ccb6355e1a08fc4a20336a66
djart <art.letter.mail@gmail.com>
2020-07-19 02:55:29 +0000

    Extended kernelmode api for Service

--- src/Kernel/lxssex/include/km/service/lxssex.Service.h
+++ src/Kernel/lxssex/include/km/service/lxssex.Service.h
@@ -3,5 +3,9 @@
 #include "../vfsplugin.h"
 #include "../../um/service/lxssex.Service.Windows.h"
 
+typedef VOID CHANNEL_CLOSING_CALLBACK(_In_ PCHANNEL Channel);
+typedef CHANNEL_CLOSING_CALLBACK* PCHANNEL_CLOSING_CALLBACK;
+
 EXTERN_C __declspec(dllexport) PCHANNEL CreateChannel(_In_ PVFS_PLUGIN Plugin, _In_ ULONG Id, _In_ SIZE_T SectionSize);
-EXTERN_C __declspec(dllexport) VOID CloseChannel(_In_ PCHANNEL Channel);
+EXTERN_C __declspec(dllexport) BOOLEAN SetChannelClosedCallback(_In_ PCHANNEL Channel, _In_ PCHANNEL_CLOSING_CALLBACK Callback);
+EXTERN_C __declspec(dllexport) BOOLEAN CloseChannel(_In_ PCHANNEL Channel);
