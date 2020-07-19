0d9313d5db1c27ebd28e5cc2187664d3110b5f99
djart <art.letter.mail@gmail.com>
2020-07-19 04:09:41 +0000

    App implemented

---
+++ src/Windows/lxssex.WindowManager/App.xaml.cs
@@ -0,0 +1,164 @@
+﻿using lxssex.WindowManager.Systems;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace lxssex.WindowManager
+{
+
+    sealed partial class App : Application
+    {
+        private readonly object StateLock = new object();
+
+        public static new App Current => (App)Application.Current;
+
+        public ApplicationState State { get; private set; } = ApplicationState.Stopped;
+
+        /// <summary>
+        /// Инициализирует одноэлементный объект приложения. Это первая выполняемая строка разрабатываемого
+        /// кода, поэтому она является логическим эквивалентом main() или WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            EnteredBackground += OnEnteredBackground;
+            LeavingBackground += OnLeavingBackground;
+            Resuming += OnResuming;
+            Suspending += OnSuspending;
+        }
+
+        private void OnEnteredBackground(object sender, EnteredBackgroundEventArgs e)
+        {
+            Deferral deferral = e.GetDeferral();
+            lock(StateLock)
+            {
+                State &= ~ApplicationState.Foreground;
+            }
+            deferral.Complete();
+        }
+
+        private void OnLeavingBackground(object sender, LeavingBackgroundEventArgs e)
+        {
+            Deferral deferral = e.GetDeferral();
+            lock (StateLock)
+            {
+                State |= ApplicationState.Foreground;
+            }
+            deferral.Complete();
+        }
+
+        /// <summary>
+        /// Вызывается в случае сбоя навигации на определенную страницу
+        /// </summary>
+        /// <param name="sender">Фрейм, для которого произошел сбой навигации</param>
+        /// <param name="e">Сведения о сбое навигации</param>
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void OnResuming(object sender, object e)
+        {
+            lock (StateLock)
+            {
+                State &= ~ApplicationState.Suspended;
+            }
+        }
+
+        /// <summary>
+        /// Вызывается при приостановке выполнения приложения.  Состояние приложения сохраняется
+        /// без учета информации о том, будет ли оно завершено или возобновлено с неизменным
+        /// содержимым памяти.
+        /// </summary>
+        /// <param name="sender">Источник запроса приостановки.</param>
+        /// <param name="e">Сведения о запросе приостановки.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            lock (StateLock)
+            {
+                State |= ApplicationState.Suspended;
+            }
+            deferral.Complete();
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            lock (StateLock)
+            {
+                State |= ApplicationState.Foreground;
+            }
+            base.OnActivated(args);
+        }
+
+        protected override async void OnBackgroundActivated(BackgroundActivatedEventArgs args)
+        {
+            lock (StateLock)
+            {
+                State |= ApplicationState.Background;
+            }
+            base.OnBackgroundActivated(args);
+            await BackgroundSystem.ProcessBackgroundTask(args.TaskInstance, args.TaskInstance.GetDeferral());
+        }
+
+        /// <summary>
+        /// Вызывается при обычном запуске приложения пользователем. Будут использоваться другие точки входа,
+        /// например, если приложение запускается для открытия конкретного файла.
+        /// </summary>
+        /// <param name="e">Сведения о запросе и обработке запуска.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            lock(StateLock)
+            {
+                State |= ApplicationState.Foreground;
+            }
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Не повторяйте инициализацию приложения, если в окне уже имеется содержимое,
+            // только обеспечьте активность окна
+            if (rootFrame == null)
+            {
+                // Создание фрейма, который станет контекстом навигации, и переход к первой странице
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Загрузить состояние из ранее приостановленного приложения
+                }
+
+                // Размещение фрейма в текущем окне
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Если стек навигации не восстанавливается для перехода к первой странице,
+                    // настройка новой страницы путем передачи необходимой информации в качестве параметра
+                    // навигации
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Обеспечение активности текущего окна
+                Window.Current.Activate();
+            }
+        }
+    }
+}
