526dbdeb5c7aecb7d29d37304eedefa7d98df195
djart <art.letter.mail@gmail.com>
2020-07-19 03:47:13 +0000

    lxssex.WindowManager.Shared added

---
+++ src/Shared/lxssex.WindowManager.Shared/WMError.cs
@@ -0,0 +1,10 @@
+﻿namespace lxssex.WindowManager.Shared
+{
+    public enum WMError : uint
+    {
+        Success = 0x0,
+        InternalError = 0xFFFFFFF,
+        WindowNotFound = 0xFFFFFFFE,
+        ArgumentException = 0xFFFFFFD,
+    }
+}
