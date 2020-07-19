48270ba72c33811e557bdd1afac2f25d75681fac
djart <art.letter.mail@gmail.com>
2020-07-19 03:49:28 +0000

    ApplicationState added

---
+++ src/Windows/lxssex.WindowManager/Systems/ApplicationState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lxssex.WindowManager.Systems
+{
+    [Flags]
+    public enum ApplicationState : uint
+    {
+        Suspended = 0x0,
+        Background = 0x1,
+        Foreground = 0x2,
+        Stopped = 0x4,
+    }
+}
